67b2a97680bb872464b23aa85e0a40fd8948005a
PhamThuong <tn.univer@gmail.com>
2025-05-24 20:35:11 +0700

    push code from local rep to remote repo

---
+++ demo7-docker-jenkins/src/test/java/com/example/demo7_docker_jenkins/Demo7DockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo7_docker_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo7DockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
